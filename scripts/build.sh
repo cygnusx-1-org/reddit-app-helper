319687dd1fa9ab88c43aa3b0f1d175d6a790f7b3
Nathan Grennan <github@cygnusx-1.org>
2025-04-21 16:45:24 -0700

    Broke the manifest out into Chrome and Firefox versions
    Added support for multiple manifest files to scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-MANIFEST_FILE="manifest.json"
+function file_list {
+  FILE_LIST=`find . -maxdepth 1 -type f -name '*.js' -o -name ${MANIFEST_FILENAME} ; find assets -type f`
+}
 
-VERSION=`cat ${MANIFEST_FILE} | jq -r '.["version"]'`
+MANIFEST_FILENAME="manifest.json"
+MANIFEST_CHROME_FILENAME="manifest-chrome.json"
+MANIFEST_FIREFOX_FILENAME="manifest-firefox.json"
+
+CHROME_VERSION=`cat ${MANIFEST_CHROME_FILENAME} | jq -r '.["version"]'`
+FIREFOX_VERSION=`cat ${MANIFEST_FIREFOX_FILENAME} | jq -r '.["version"]'`
 
 BUILD_DIR="builds"
-ZIP_FILENAME="${BUILD_DIR}/reddit-app-helper-${VERSION}.zip"
-XPI_FILENAME="${BUILD_DIR}/reddit-app-helper-${VERSION}.xpi"
 
-FILE_LIST=`find . -maxdepth 1 -type f -name '*.js' -o -name '*.json' ; find assets -type f`
+ZIP_FILENAME="${BUILD_DIR}/reddit-app-helper-${CHROME_VERSION}.zip"
+XPI_FILENAME="${BUILD_DIR}/reddit-app-helper-${FIREFOX_VERSION}.xpi"
+
+# Chrome
+mv ${MANIFEST_CHROME_FILENAME} ${MANIFEST_FILENAME}
+
+file_list
 
 zip -q ${ZIP_FILENAME} ${FILE_LIST}
 
-cp -a ${ZIP_FILENAME} ${XPI_FILENAME}
+mv ${MANIFEST_FILENAME} ${MANIFEST_CHROME_FILENAME}
+
+# Firefox
+mv ${MANIFEST_FIREFOX_FILENAME} ${MANIFEST_FILENAME}
+
+file_list
+
+zip -q ${XPI_FILENAME} ${FILE_LIST}
+
+mv ${MANIFEST_FILENAME} ${MANIFEST_FIREFOX_FILENAME}
